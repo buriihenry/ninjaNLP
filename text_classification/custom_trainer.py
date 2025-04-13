7f849c9423406ef57a6166cc18b84e84e350bea2
buriihenry <buriihenry@gmail.com>
2025-04-14 02:40:27 +0300

    refactor code

--- text_classification/custom_trainer.py
+++ text_classification/custom_trainer.py
@@ -1,27 +1,32 @@
 import torch
 from torch import nn
-
-from transformers import Trainer, TrainingArguments
+from transformers import Trainer
 
 class CustomTrainer(Trainer):
-
+    
     def compute_loss(self, model, inputs, return_outputs=False):
         labels = inputs.get("labels")
-
-        #forward pass
+        
+        # Forward pass
         outputs = model(**inputs)
         logits = outputs.get("logits")
-
-        # Compute Custom Loss
-        loss_fct = nn.CrossEntropyLoss(weight = torch.tensor(self.class_weights).to(device=self.device))
-        loss = loss_fct(logits.view(-1,self.model.config.num_labels),labels.view(-1))
-
+        
+        # Convert class weights to Float (32-bit)
+        weights = torch.tensor(self.class_weights, dtype=torch.float32).to(device=self.device)
+        
+        # Compute Custom Loss (ensure consistent types)
+        loss_fct = nn.CrossEntropyLoss(weight=weights)
+        
+        # Make sure logits and labels are both Float32
+        logits = logits.to(torch.float32)
+        labels = labels.to(torch.int64)  # CrossEntropyLoss expects Long type labels
+        
+        loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
+        
         return (loss, outputs) if return_outputs else loss
     
     def set_class_weights(self, class_weights):
         self.class_weights = class_weights
-
+    
     def set_device(self, device):
-        self.device = device    
-
- 
+        self.device = device
